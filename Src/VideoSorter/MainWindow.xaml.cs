0fff486eaea3e1c2beeab8cbe44179e669d0f207
AlexPi <alex.pigida@outlook.com>
2021-06-06 17:42:32 -0400

    scaffolded a 1st draft/POC

---
+++ Src/VideoSorter/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VideoSorter.Views;
+
+namespace VideoSorter
+{
+  public partial class MainWindow : Window
+  {
+    const int _max = 6;
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      int i = 0;
+      foreach (var filename in Directory.GetFiles(@"C:\Users\alexp\OneDrive\Pictures\Camera Roll 1\SurfSkate", "*.mp4"))
+      {
+        if (++i > _max) break;
+
+        wp1.Children.Add(new VideoUC { VideoFile = filename });
+      }
+    }
+
+    void onTglPlay(object sender, RoutedEventArgs e) { foreach (VideoUC vp in wp1.Children) { vp.IsPlaying = !vp.IsPlaying; } }
+    void onToStart(object sender, RoutedEventArgs e) { foreach (VideoUC vp in wp1.Children) { vp.Restart(); } }
+    void onClose(object sender, RoutedEventArgs e) { Close(); ; }
+
+    void Button_Click(object sender, RoutedEventArgs e) { }
+  }
+}
---
+++ Src/VideoSorter/Views/VideoUC.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoSorter.Views
+{
+  public partial class VideoUC : UserControl
+  {
+    string _vf;
+    bool _isplaying;
+
+    public VideoUC()
+    {
+      InitializeComponent();
+    }
+    public VideoUC(string item) : this()
+    {
+      _vf = item;
+    }
+    void onLoaded(object sender, RoutedEventArgs e)
+    {
+      me1.ToolTip = 
+      tbkFilename.Text = System.IO.Path.GetFileNameWithoutExtension(_vf);
+
+      me1.Source = new Uri(_vf);
+    }
+
+    public string VideoFile { get => _vf; internal set => tbkFilename.Text = _vf = value; }
+    public bool IsPlaying
+    {
+      get => _isplaying;
+      set
+      {
+        _isplaying = value;
+        if (value) me1.Play(); else me1.Pause();
+      }
+    }
+
+    internal void Restart()
+    {
+      me1.Stop();
+      me1.Play();
+    }
+
+    private void Bold_Checked(object sender, RoutedEventArgs e)
+    {
+      tbkFilename.FontWeight = FontWeights.Bold;
+    }
+
+    private void Bold_Unchecked(object sender, RoutedEventArgs e)
+    {
+      tbkFilename.FontWeight = FontWeights.Normal;
+    }
+
+    private void Italic_Checked(object sender, RoutedEventArgs e)
+    {
+      tbkFilename.FontStyle = FontStyles.Italic;
+    }
+
+    private void Italic_Unchecked(object sender, RoutedEventArgs e)
+    {
+      tbkFilename.FontStyle = FontStyles.Normal;
+    }
+
+    private void IncreaseFont_Click(object sender, RoutedEventArgs e)
+    {
+      if (tbkFilename.FontSize < 18)
+      {
+        tbkFilename.FontSize += 2;
+      }
+    }
+
+    private void DecreaseFont_Click(object sender, RoutedEventArgs e)
+    {
+      if (tbkFilename.FontSize > 10)
+      {
+        tbkFilename.FontSize -= 2;
+      }
+    }
+  }
+}
