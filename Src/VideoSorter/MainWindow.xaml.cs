5c69d0f065abd71b29d22232f244980de4a2c01d
AlexPi <alex.pigida@outlook.com>
2021-06-06 20:58:27 -0400

    progress - for real

--- Src/VideoSorter/MainWindow.xaml.cs
+++ Src/VideoSorter/MainWindow.xaml.cs
@@ -1,45 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using VideoSorter.Views;
 
 namespace VideoSorter
 {
   public partial class MainWindow : Window
   {
-    const int _max = 16;
-    public MainWindow()
-    {
-      InitializeComponent();
-    }
+    const int _max = 96;
+    public MainWindow() => InitializeComponent();
 
-    void Window_Loaded(object sender, RoutedEventArgs e)
+    async void Window_Loaded(object sender, RoutedEventArgs e)
     {
-      int i = 0;
+      var i = 0;
       foreach (var filename in Directory.GetFiles(@"C:\Users\alexp\OneDrive\Pictures\Camera Roll 1\SurfSkate", "*.mp4"))
       {
         if (++i > _max) break;
 
-        wp1.Children.Add(new VideoUC { VideoFile = filename });
+        _ = wp1.Children.Add(new VideoUC(filename)); // { VideoFile = filename });// ;
+        await Task.Delay(200);
       }
     }
 
     void onTglPlay(object sender, RoutedEventArgs e) { foreach (VideoUC vp in wp1.Children) { vp.IsPlaying = !vp.IsPlaying; } }
-    void onToStart(object sender, RoutedEventArgs e) { foreach (VideoUC vp in wp1.Children) { vp.Restart(); } }
+    void onToStart(object sender, RoutedEventArgs e) { foreach (VideoUC vp in wp1.Children) { vp.RestartFromBegining(); } }
+    void onPausAll(object sender, RoutedEventArgs e) { foreach (VideoUC vp in wp1.Children) { vp.Pause(); } }
     void onClose(object sender, RoutedEventArgs e) { Close(); ; }
 
     void Button_Click(object sender, RoutedEventArgs e) { }
+
+    void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
   }
 }
--- Src/VideoSorter/Views/VideoUC.xaml.cs
+++ Src/VideoSorter/Views/VideoUC.xaml.cs
@@ -1,62 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VideoSorter.Views
 {
   public partial class VideoUC : UserControl
   {
     string _vf;
-    bool _isplaying;
-    bool _isActive = true;
+    bool _isplaying, _isActive;
 
-    public VideoUC()
+    public VideoUC() => InitializeComponent();
+    public VideoUC(string item) : this() => _vf = item;
+    async void onLoaded(object sender, RoutedEventArgs e)
     {
-      InitializeComponent();
-    }
-    public VideoUC(string item) : this()
-    {
-      _vf = item;
-    }
-    void onLoaded(object sender, RoutedEventArgs e)
-    {
-      me1.ToolTip = 
+      me1.ToolTip =
       tbkFilename.Text = System.IO.Path.GetFileNameWithoutExtension(_vf);
 
       me1.Source = new Uri(_vf);
+      me1.Play();
+      await Task.Delay(100);
+      me1.Pause();
     }
 
     public string VideoFile { get => _vf; internal set => tbkFilename.Text = _vf = value; }
-    public bool IsPlaying
-    {
-      get => _isplaying;
-      set
-      {
-        _isplaying = value;
-        if (value) me1.Play(); else me1.Pause();
-      }
-    }
-
-
     public bool IsAcitve
     {
-      get { return _isActive; }
+      get => _isActive;
       set
       {
         _isActive = value;
         if (value)
+        {
           pnlFilename.Visibility = Visibility.Collapsed;
+          me1.Play();
+        }
         else
         {
           pnlFilename.Visibility = Visibility.Visible;
@@ -64,54 +43,21 @@ namespace VideoSorter.Views
         }
       }
     }
+    public bool IsPlaying { get => _isplaying; set { _isplaying = value; if (value && IsAcitve) me1.Play(); else me1.Pause(); } }
 
+    internal void Pause() { _isplaying = false; me1.Pause(); }
+    internal void RestartFromBegining() { me1.Stop(); if (IsAcitve) me1.Play(); }
 
-    internal void Restart()
-    {
-      me1.Stop();
-      me1.Play();
-    }
+    void Bold_Checked(object sender, RoutedEventArgs e) => tbkFilename.FontWeight = FontWeights.Bold;
+    void Bold_Unchecked(object sender, RoutedEventArgs e) => tbkFilename.FontWeight = FontWeights.Normal;
+    void IncreaseFont_Click(object sender, RoutedEventArgs e) { if (tbkFilename.FontSize < 18) { tbkFilename.FontSize += 2; } }
+    void DecreaseFont_Click(object sender, RoutedEventArgs e) { if (tbkFilename.FontSize > 10) { tbkFilename.FontSize -= 2; } }
 
-    private void Bold_Checked(object sender, RoutedEventArgs e)
-    {
-      tbkFilename.FontWeight = FontWeights.Bold;
-    }
+    void me1_Loaded(object sender, RoutedEventArgs e) { if (me1.NaturalDuration.HasTimeSpan) tbkDuration.Text = $"{me1.NaturalDuration.TimeSpan:mm\\:ss} Loed"; }
+    void me1_MediaOpened(object sender, RoutedEventArgs e) { if (me1.NaturalDuration.HasTimeSpan) tbkDuration.Text = $" {me1.NaturalDuration.TimeSpan.TotalSeconds:N0} "; }
+    void me1_MediaFailed(object sender, ExceptionRoutedEventArgs e) => tbkDuration.Text = $"{e.ErrorException.Message}";
 
-    private void Bold_Unchecked(object sender, RoutedEventArgs e)
-    {
-      tbkFilename.FontWeight = FontWeights.Normal;
-    }
-
-    private void Italic_Checked(object sender, RoutedEventArgs e)
-    {
-      tbkFilename.FontStyle = FontStyles.Italic;
-    }
-
-    private void Italic_Unchecked(object sender, RoutedEventArgs e)
-    {
-      tbkFilename.FontStyle = FontStyles.Normal;
-    }
-
-    private void IncreaseFont_Click(object sender, RoutedEventArgs e)
-    {
-      if (tbkFilename.FontSize < 18)
-      {
-        tbkFilename.FontSize += 2;
-      }
-    }
-
-    private void DecreaseFont_Click(object sender, RoutedEventArgs e)
-    {
-      if (tbkFilename.FontSize > 10)
-      {
-        tbkFilename.FontSize -= 2;
-      }
-    }
-
-    void onMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-      IsAcitve = !IsAcitve;
-
-    }
+    void onMouseLeftButtonDown(object sender, MouseButtonEventArgs e) => IsAcitve = !IsAcitve;
+    void onMouseDoubleClick(object sender, MouseButtonEventArgs e) => RestartFromBegining();
   }
 }
