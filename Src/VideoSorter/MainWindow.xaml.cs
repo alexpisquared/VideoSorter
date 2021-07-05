58d55f88a1dd4da0be75f60353b687cd976a7ba0
AlexPi <alex.pigida@outlook.com>
2021-07-05 13:22:31 -0400

    ++

--- Src/VideoSorter/MainWindow.xaml.cs
+++ Src/VideoSorter/MainWindow.xaml.cs
@@ -30,8 +30,11 @@ namespace VideoSorter
 #endif
       var srcDir = Environment.GetCommandLineArgs().Length > 1 ? Environment.GetCommandLineArgs()[1] : @"C:\Users\alexp\OneDrive\Pictures\Camera Roll 1";
 
-      foreach (var sfx in _targetDirSuffixes) if (!Directory.Exists(Path.Combine(srcDir, sfx))) Directory.CreateDirectory(Path.Combine(srcDir, sfx));
+      await tryLoadVideoFiles(srcDir);
+    }
 
+    async Task tryLoadVideoFiles(string srcDir)
+    {
       var videoFiles = Directory.GetFiles(srcDir, "*.mp4");
       if (videoFiles.Length == 0)
       {
@@ -47,9 +50,13 @@ namespace VideoSorter
         {
           srcDir = dir;
           videoFiles = Directory.GetFiles(srcDir, "*.mp4");
-          if (videoFiles.Length != 0)
+          if (videoFiles.Length == 0)
+            tbkInfo.Text += $"No files in \t {srcDir}.\n";
+          else
+          {
+            tbkInfo.Text += $"{videoFiles.Length} files in \t {srcDir}.\n";
             goto rrr;
-          tbkInfo.Text += $"No files in \n\t {srcDir}.\n";
+          }
         }
 
         tbkInfo.Text = $"No files in subfolders  of \n\t {srcDir}.";
@@ -58,25 +65,45 @@ namespace VideoSorter
       }
 
       rrr:
-      var i = 0;
+      foreach (var sfx in _targetDirSuffixes) if (!Directory.Exists(Path.Combine(srcDir, sfx))) Directory.CreateDirectory(Path.Combine(srcDir, sfx));
+      var loadCount = 0;
       foreach (var filename in videoFiles.OrderBy(r => r))
       {
-        if (++i > _max) break;
+        if (++loadCount > _max) break;
 
         wrapPnl.Children.Add(new VideoUC(filename, _targetDirSuffixes));
 
-        tbkReport.Text = $"  {i} / {videoFiles.Length} files  ";
+        tbkReport.Text = $"  {loadCount} / {videoFiles.Length} files  ";
 
         await Task.Delay(300);
       }
 
-      tbkReport.Text = $"  {i} files loaded out of {videoFiles.Length}. ";
+      tbkReport.Text = $"  {loadCount} files loaded out of {videoFiles.Length}. ";
 
       System.Media.SystemSounds.Asterisk.Play();
     }
+
     void onTglPlay(object s, RoutedEventArgs e) { foreach (VideoUC vp in wrapPnl.Children) { /*vp.IsPlayingAll = !vp.IsPlayingAll*/; } }
     void onToStart(object s, RoutedEventArgs e) { foreach (VideoUC vp in wrapPnl.Children) { vp.RestartFromBegining(); } }
     void onPausAll(object s, RoutedEventArgs e) { foreach (VideoUC vp in wrapPnl.Children) { vp.paus(); } }
     void onClose(object s, RoutedEventArgs e) { Close(); ; }
+
+    void Window_DragOver(object sender, DragEventArgs e)
+    {
+
+    }
+
+    async void Window_Drop(object sender, DragEventArgs e)
+    {
+      if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+      var files = e.Data.GetData(DataFormats.FileDrop) as string[];
+      if (files?.Length < 1) return;
+
+      var csv = string.Join("|", files);
+
+      var srcDir = files.First();
+
+      await tryLoadVideoFiles(srcDir);
+    }
   }
 }
